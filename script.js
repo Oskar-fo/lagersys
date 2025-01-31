f01c82747580ba6b5754469de71b549ae56d1b48
Oskar-fo <oskar.folling@gmail.com>
2025-01-31 13:19:15 +0100

    :)

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ document.getElementById("leggTil").addEventListener("click", function () {
         let liste = document.getElementById("liste");
         let li = document.createElement("li");
         li.textContent = capitalizeFirstLetter(nyttElement);
+
+        let toggleButton = document.createElement("button");
+        toggleButton.textContent = "Ledig";
+        toggleButton.style.marginLeft = "50px";
+        toggleButton.style.width= "200px";
+        toggleButton.addEventListener("click", function () {
+            if (toggleButton.textContent === "Ledig") {
+                toggleButton.textContent = "Utilgjengelig";
+                li.style.color = "red";
+            } else {
+                toggleButton.textContent = "Ledig";
+                li.style.color = "black";
+            }
+        });
+
+        li.appendChild(toggleButton);
         liste.appendChild(li);
         document.getElementById("nyttElement").value = "";
     } else {
@@ -19,20 +35,12 @@ document.getElementById("leggTil").addEventListener("click", function () {
 document.getElementById("sorter").addEventListener("click", function () {
     let listeElementer = document.querySelectorAll("#liste li");
 
-    let sortertArray = Array.from(listeElementer)
-        .map(li => li.textContent)
-        .sort();
+    let sortertArray = Array.from(listeElementer).sort((a, b) =>
+        a.textContent.localeCompare(b.textContent)
+    );
 
     let liste = document.getElementById("liste");
     liste.innerHTML = "";
 
-    sortertArray.forEach(tekst => {
-        let nyttElement = document.createElement("li");
-        nyttElement.textContent = tekst;
-        liste.appendChild(nyttElement);
-    });
+    sortertArray.forEach(li => liste.appendChild(li));
 });
-
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
